6f37904a814dd8ad17e31b172a48da6707bc724c
Edi Modrić <emodric@users.noreply.github.com>
2017-11-04 01:24:24 +0100

    Only add namespaced templates to path registry

--- lib/Templating/Twig/TwigThemeLoader.php
+++ lib/Templating/Twig/TwigThemeLoader.php
@@ -68,8 +68,12 @@ class TwigThemeLoader extends FilesystemLoader implements Twig_LoaderInterface, 
 
     public function getSourceContext($name)
     {
-        $source = $this->innerFilesystemLoader->getSourceContext($this->nameResolver->resolveTemplateName((string)$name));
-        $this->pathRegistry->mapTemplatePath($source->getName(), $source->getPath());
+        $name = (string)$name;
+        $source = $this->innerFilesystemLoader->getSourceContext($this->nameResolver->resolveTemplateName($name));
+
+        if ($this->nameResolver->isTemplateDesignNamespaced($name)) {
+            $this->pathRegistry->mapTemplatePath($source->getName(), $source->getPath());
+        }
 
         return $source;
     }
